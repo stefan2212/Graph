396f2c1ded963d68813a7c94d01ee8c487806d40
stefan2212 <stefan.cernescu94@gmail.com>
2017-04-15 18:25:52 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,93 @@
+import java.util.ArrayList;
+
+class Graph{
+
+    private ArrayList<Node> noduri;
+    private ArrayList<Edge> muchii;
+
+    public Graph(){
+        noduri=new ArrayList<Node>();
+        muchii=new ArrayList<Edge>();
+    }
+
+    public void AddNode(int x, int y,  int value){
+        Node newNode = new Node();
+        newNode.x = x;
+        newNode.y = y;
+        newNode.Value = value;
+        noduri.add(newNode);
+    }
+    public boolean AddEdge(int IndexNode1, int IndexNode2){
+        if(IndexNode1==IndexNode2)
+            return false;
+        for(Edge edge:muchii)
+        {
+            if(edge.IndexNode1 == IndexNode1 && edge.Index2Node2 == IndexNode2)
+                return false;
+        }
+        Edge edge = new Edge();
+        edge.IndexNode1 = IndexNode1;
+        edge.Index2Node2 = IndexNode2;
+        muchii.add(edge);
+        return true;
+    }
+    public int GetNodesCount(){
+        return noduri.size();
+    }
+    public int GetEdgesCount(){
+        return muchii.size();
+    }
+
+    public Node GetNode(int index){
+        return noduri.get(index);
+    }
+
+    public boolean isConex(){
+        int nodAles=0;
+        int [] culori = new int[noduri.size()];
+        for(int i=0;i<noduri.size();i++)
+            culori[i]=-1;
+        int k=0;
+        culori[nodAles]=0;
+        while(k<muchii.size()) {
+            for (Edge edge : muchii) {
+                if (edge.IndexNode1 == nodAles && culori[edge.Index2Node2] == -1) {
+                    culori[edge.Index2Node2] = 0;
+                    nodAles = edge.Index2Node2;
+                }
+            }
+            k++;
+        }
+        for(int i=0;i<noduri.size();i++)
+            if(culori[i]==-1)
+                return false;
+        return true;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Graph g=new Graph();
+        g.AddNode(10,10,100);
+        g.AddNode(10,20,200);
+        g.AddNode(20,20,300);
+        g.AddEdge(0, 1);
+        g.AddEdge(1, 2);
+
+        System.out.printf("Node 1 value is %d \n",g.GetNode(1).Value);
+        g.AddNode(0,0,200);
+        System.out.printf("Nodes = %d, Edges=%d, Conex=%b\n", g.GetNodesCount(), g.GetEdgesCount(), g.isConex());
+    }
+}
+
+class Node{
+    public int x;
+    public int y;
+    public int Value;
+}
+
+class Edge{
+    public int IndexNode1;
+    public int Index2Node2;
+}
